61e191302ac3d5f0d9e6a1cd7f124e5d1c4a5de2
fuzzlepuzzle <70248630+fuzzlepuzzle@users.noreply.github.com>
2026-07-14 13:20:21 -0700

    Moving Stained Glass frame in preparation of improving it.

--- js/frames/packStainedGlass.js
+++ js/frames/packStainedGlass.js
@@ -5,25 +5,25 @@ var bounds = {x:0.7747, y:0.8872, width:0.1747, height:0.0605};
 var bounds2 = {x:0.1134, y:0.0139, width:0.7734, height:0.0262};
 //defines available frames
 availableFrames = [
-	{name:'White Frame', src:'/img/frames/dmu/stainedGlass/w.png', masks:masks},
-	{name:'Blue Frame', src:'/img/frames/dmu/stainedGlass/u.png', masks:masks},
-	{name:'Black Frame', src:'/img/frames/dmu/stainedGlass/b.png', masks:masks},
-	{name:'Red Frame', src:'/img/frames/dmu/stainedGlass/r.png', masks:masks},
-	{name:'Green Frame', src:'/img/frames/dmu/stainedGlass/g.png', masks:masks},
-	{name:'Multicolored Frame', src:'/img/frames/dmu/stainedGlass/m.png', masks:masks},
-	{name:'White Power/Toughness', src:'/img/frames/dmu/stainedGlass/pt/w.png', bounds:bounds},
-	{name:'Blue Power/Toughness', src:'/img/frames/dmu/stainedGlass/pt/u.png', bounds:bounds},
-	{name:'Black Power/Toughness', src:'/img/frames/dmu/stainedGlass/pt/b.png', bounds:bounds},
-	{name:'Red Power/Toughness', src:'/img/frames/dmu/stainedGlass/pt/r.png', bounds:bounds},
-	{name:'Green Power/Toughness', src:'/img/frames/dmu/stainedGlass/pt/g.png', bounds:bounds},
-	{name:'Multicolored Power/Toughness', src:'/img/frames/dmu/stainedGlass/pt/m.png', bounds:bounds},
-	{name:'White Legend Crown', src:'/img/frames/dmu/stainedGlass/crowns/w.png', bounds:bounds2, masks:masks2},
-	{name:'Blue Legend Crown', src:'/img/frames/dmu/stainedGlass/crowns/u.png', bounds:bounds2, masks:masks2},
-	{name:'Black Legend Crown', src:'/img/frames/dmu/stainedGlass/crowns/b.png', bounds:bounds2, masks:masks2},
-	{name:'Red Legend Crown', src:'/img/frames/dmu/stainedGlass/crowns/r.png', bounds:bounds2, masks:masks2},
-	{name:'Green Legend Crown', src:'/img/frames/dmu/stainedGlass/crowns/g.png', bounds:bounds2, masks:masks2},
-	{name:'Multicolored Legend Crown', src:'/img/frames/dmu/stainedGlass/crowns/m.png', bounds:bounds2, masks:masks2},
-	{name:'Rare Stamp', src:'/img/frames/dmu/stamp.png', bounds:{x:0.4554, y:0.9172, width:0.0894, height:0.0320}}
+	{name:'White Frame', src:'/img/frames/stainedGlass/w.png', masks:masks},
+	{name:'Blue Frame', src:'/img/frames/stainedGlass/u.png', masks:masks},
+	{name:'Black Frame', src:'/img/frames/stainedGlass/b.png', masks:masks},
+	{name:'Red Frame', src:'/img/frames/stainedGlass/r.png', masks:masks},
+	{name:'Green Frame', src:'/img/frames/stainedGlass/g.png', masks:masks},
+	{name:'Multicolored Frame', src:'/img/frames/stainedGlass/m.png', masks:masks},
+	{name:'White Power/Toughness', src:'/img/frames/stainedGlass/pt/w.png', bounds:bounds},
+	{name:'Blue Power/Toughness', src:'/img/frames/stainedGlass/pt/u.png', bounds:bounds},
+	{name:'Black Power/Toughness', src:'/img/frames/stainedGlass/pt/b.png', bounds:bounds},
+	{name:'Red Power/Toughness', src:'/img/frames/stainedGlass/pt/r.png', bounds:bounds},
+	{name:'Green Power/Toughness', src:'/img/frames/stainedGlass/pt/g.png', bounds:bounds},
+	{name:'Multicolored Power/Toughness', src:'/img/frames/stainedGlass/pt/m.png', bounds:bounds},
+	{name:'White Legend Crown', src:'/img/frames/stainedGlass/crowns/w.png', bounds:bounds2, masks:masks2},
+	{name:'Blue Legend Crown', src:'/img/frames/stainedGlass/crowns/u.png', bounds:bounds2, masks:masks2},
+	{name:'Black Legend Crown', src:'/img/frames/stainedGlass/crowns/b.png', bounds:bounds2, masks:masks2},
+	{name:'Red Legend Crown', src:'/img/frames/stainedGlass/crowns/r.png', bounds:bounds2, masks:masks2},
+	{name:'Green Legend Crown', src:'/img/frames/stainedGlass/crowns/g.png', bounds:bounds2, masks:masks2},
+	{name:'Multicolored Legend Crown', src:'/img/frames/stainedGlass/crowns/m.png', bounds:bounds2, masks:masks2},
+	{name:'Rare Stamp', src:'/img/frames/m15/holoStamps.png', bounds:{x:0.4554, y:0.9172, width:0.0894, height:0.0320}}
 ];
 //disables/enables the "Load Frame Version" button
 document.querySelector('#loadFrameVersion').disabled = false;
@@ -32,7 +32,7 @@ document.querySelector('#loadFrameVersion').onclick = async function() {
 	//resets things so that every frame doesn't have to
 	await resetCardIrregularities();
 	//sets card version
-	card.version = 'dmuStainedGlass';
+	card.version = 'stainedGlass';
 	//art bounds
 	card.artBounds = {x:0.04, y:0.1105, width:0.92, height:0.4462};
 	autoFitArt();
